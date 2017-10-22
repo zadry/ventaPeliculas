3f042addaff78f31ab23d96001f6601bde9ecd32
zadry <armoniadelaberintos@gmail.com>
2017-10-22 14:29:07 -0500

    Mostrar total agregado, boton "confirmar compra" agregado y funcionando

--- ventaPeliculas/DetallesCarrito.cs
+++ ventaPeliculas/DetallesCarrito.cs
@@ -13,6 +13,7 @@ namespace ventaPeliculas
     public partial class DetallesCarrito : Form
     {
         private ArrayList carrCom;
+        private double total = 0;
 
         public DetallesCarrito()
         {
@@ -26,6 +27,8 @@ namespace ventaPeliculas
 
             InitializeComponent();
             fillCarrito();
+            calcularTotal();
+            label2.Text = "Total: $" + total;
             //carrCom = new ArrayList();
         }
 
@@ -40,12 +43,39 @@ namespace ventaPeliculas
             }
         }
 
+        /*
+         *  Calcular total
+         */
+
+        private void calcularTotal()
+        {
+            for (int i = 0; i < carrCom.Count; i++)
+            {
+                total = total + ((Peliculas)carrCom[i]).getPrecio();
+            }
+        }
+
         private void DetallesCarrito_Load(object sender, EventArgs e)
         {
         }
 
         private void listPelic_SelectedIndexChanged(object sender, EventArgs e)
         {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult res2 = MessageBox.Show("¿Seguro que quiere realizar esta compra?", "Agregar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res2 == DialogResult.Yes)
+            {
+                carrCom = new ArrayList();
+                listPelic.Items.Clear();
+                fillCarrito();
+                label2.Text = "Su compra fue realizada con EXITO";
+            }
+            else
+            {
+            }
         }
     }
 }
